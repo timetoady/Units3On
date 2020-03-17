d37c42b42dba732d7c677ac5478886de7ddef261
ADAM ANDREASON <adamandreason@gmail.com>
2020-03-17 04:06:44 -0600

    all required seemingly accomplished

--- Unit10/swagshop.js
+++ Unit10/swagshop.js
@@ -1,12 +1,39 @@
 function myFunction() {
-    let x = document.querySelector("#myLinks");
-    let hamburger = document.querySelector("#menu");
+  let x = document.querySelector("#myLinks");
+  let hamburger = document.querySelector("#menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    hamburger.setAttribute("class", "fa fa-bars");
+  } else {
+    x.style.display = "block";
+    hamburger.setAttribute("class", "fas fa-times");
+  }
+}
+
+function myFunction2() {
+  let x = document.querySelector("#orderSublink");
+  if (window.screen.width <= 560) {
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+}
+
+function myFunction3() {
+  let x = document.querySelector("#orderSublink");
+  let mover = document.querySelector("#fakeIt");
+  let origin = document.querySelector("#origin")
+  if (window.screen.width >= 561) {
+    
+    mover.appendChild(x);
     if (x.style.display === "block") {
       x.style.display = "none";
-      hamburger.setAttribute("class", "fa fa-bars");
     } else {
       x.style.display = "block";
-      hamburger.setAttribute("class", "fas fa-times");
     }
+  } else {
+    origin.appendChild(x);
   }
-  
+}
