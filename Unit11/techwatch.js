958f55ef71e45f88b8b2b22bc65820679b18b0ea
ADAM ANDREASON <adamandreason@gmail.com>
2020-03-20 01:54:08 -0600

    initial setup of basic parts

---
+++ Unit11/techwatch.js
@@ -0,0 +1,12 @@
+// function myFunction() {
+//     let x = document.querySelector("#myLinks");
+//     let hamburger = document.querySelector("#menu");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//       hamburger.setAttribute("class", "fa fa-bars");
+//     } else {
+//       x.style.display = "block";
+//       hamburger.setAttribute("class", "fas fa-times");
+//     }
+//   }
+  
