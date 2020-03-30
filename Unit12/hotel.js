2962396a2db4dd3848a6a8a62c1dba9234e337cc
ADAM ANDREASON <adamandreason@gmail.com>
2020-03-29 22:39:41 -0600

    basic requirments met, styling begun

--- Unit12/hotel.js
+++ Unit12/hotel.js
@@ -30,32 +30,5 @@ fetch(apiURL)
       });
 });
 
-// function hotelInfo(info) {
-//     let loadHotel = info.hotels;
-//     loadHotel.forEach(hotel => {
-//     let mainArea = document.querySelector('main');
-//     let hotelSec = document.createElement("section");
-//     let hotelFig = document.createElement("figure");
-//     let hotelImg = document.createElement("img");
-//     let hotelName = document.createElement("figcaption");
-//     let hotelDiv = document.createElement("div");
-//     let address = document.createElement("p");
-//     let phone = document.createElement("p");
-//     hotelImg.src = `/Unit12/assets/${hotel.picture}`;
-//     hotelName.textContent = hotel.name;
-//     phone.innerHTML = `<ion-icon name="call"></ion-icon> ${hotel.phoneNum}`;
-//     address.innerHTML = `<ion-icon name="map"></ion-icon> ${hotel.address}`;
-//     mainArea.appendChild(hotelDiv);
-//     hotelSec.appendChild(hotelFig);
-//     hotelFig.appendChild(hotelImg);
-//     hotelFig.appendChild(hotelName);
-//     hotelSec.appendChild(hotelDiv);
-//     hotelDiv.appendChild(address);
-//     hotelDiv.appendChild(phone);
-//     hotelDiv.setAttribute('class', "hotel")
-//   });
-// }
-
-
 
 
---
+++ weather.js
@@ -0,0 +1,66 @@
+const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=1838524&appid=13521782592d85bd681b33eaba1681c2&units=imperial"
+const myDate = new Date();
+const currentDayNum = myDate.getDay();
+console.log(currentDayNum)
+let forcastDayNumber = currentDayNum
+const weekdays = new Array(7);
+weekdays[0] = "Sunday";
+weekdays[1] = "Monday";
+weekdays[2] = "Tuesday";
+weekdays[3] = "Wednesday";
+weekdays[4] = "Thursday";
+weekdays[5] = "Friday";
+weekdays[6] = "Saturday";
+
+
+fetch(apiURL)
+  .then(weatherData => weatherData.json())
+  .then(weatherData => {
+    let name = document.querySelector("#townName");
+    name.textContent = weatherData.city.name;
+    console.log(weatherData);
+    console.log(name);
+    // const iconCode = weatherData.list[0].weather[0].icon;
+    weatherData.list.forEach(report => {
+        var time = report.dt_txt;
+        const iconCode = report.weather[0].icon;
+        if (time.includes('03:00:00')){
+            forcastDayNumber += 1;
+            if (forcastDayNumber === 7) {
+                forcastDayNumber = 0;
+            }
+            let forcast = document.querySelector("#forcast");
+            let dayDiv = document.createElement('div');
+            let dayNameDiv = document.createElement('div');
+            let dayName = document.createElement('h2');
+            let theTemp = document.createElement('h3');
+            let icon = document.createElement('img');
+            dayDiv.setAttribute("class", "dayDiv");
+            dayNameDiv.setAttribute('class', 'dayNameDiv')
+            theTemp.innerHTML = `${report.main.temp}&deg F`
+            dayName.textContent = weekdays[forcastDayNumber];
+            icon.src = `//openweathermap.org/img/w/${iconCode}.png`
+            forcast.appendChild(dayDiv);
+            dayDiv.appendChild(dayNameDiv);
+            dayNameDiv.appendChild(dayName);
+            forcast.appendChild(theTemp);
+            forcast.appendChild(icon)
+            console.log(`The noontime temperature in ${name.textContent} for ${weekdays[forcastDayNumber]} is ${report.main.temp}° F`)
+
+        }
+    })
+
+
+    // console.log(iconCode)
+    // const iconPath = `//openweathermap.org/img/w/${iconCode}.png`;
+    // console.log(iconPath);
+    // let imgTest = document.querySelector("#imageTest");
+    // let showImg = document.createElement('img');
+    // showImg.src = iconPath;
+    
+    // imgTest.appendChild(showImg)
+
+  });
+
+
+  // `//openweathermap.org/img/w/${iconImg}.png`
